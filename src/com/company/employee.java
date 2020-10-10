16c31f91ea11e6b4b16c19b785b647be43890beb
Nachiket13 <ngsathvara1218@gmail.com>
2020-10-10 20:04:12 +0530

    Initial commit

---
+++ src/com/company/employee.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public  class employee {
+    String name="Nachiket", city="Ahmedabad";
+    int age=21;
+
+    public void displayData(){
+        System.out.println("The Name is:\n" + name + "\nCity is:\n" + city + "\nAge is:\n" + age);
+    }
+}
